a8e9812562a8b9da576e1dbcc4c776e419a177a7
RUBEN DAVID ZAPATA BRAVO <rubenzapata264702@correo.itm.edu.co>
2024-12-01 14:55:19 -0500

    ICountryService.cs Created

--- DAL/Entities/Country.cs
+++ DAL/Entities/Country.cs
@@ -4,7 +4,7 @@ namespace webapitest.DAL.Entities
 {
     public class Country : AuditBase
     {
-        [Display(Name = "Nombre")]
+        [Display(Name = "Pais")]
         [MaxLength(50, ErrorMessage = "El Campo {0} excede el limite de caracteres")]
         [Required(ErrorMessage = "El Campo {0} es obligatorio")]
         public string Name { get; set; }
---
+++ Domain/Interfaces/ICountryService.cs
@@ -0,0 +1,17 @@
+﻿using webapitest.DAL.Entities;
+
+namespace webapitest.Domain.Interfaces
+{
+    public interface ICountryService
+    {
+        Task<IEnumerable<Country>> GetCountryAsync();
+
+        Task<Country> CreateCountryAsync(Country country);
+
+        Task<Country> GetCountryByIdAsync(Guid Id);
+
+        Task<Country> EditCountryAsync(Country country);
+
+        Task<Country> DeleteCountryAsync(Guid Id);
+    }
+}
