8a3cd673a54a7cd1ccf0ce257563f3d31c2fcc6c
RUBEN DAVID ZAPATA BRAVO <rubenzapata264702@correo.itm.edu.co>
2024-12-01 16:02:51 -0500

    CountriesController Created

---
+++ Controllers/CountriesController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webapitest.DAL.Entities;
+using webapitest.Domain.Interfaces;
+using webapitest.Domain.Services;
+
+namespace webapitest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController:Controller
+    {
+        private readonly ICountryService _CountryService;
+
+
+        public CountriesController(ICountryService countryService)
+        {
+            _CountryService = countryService;
+
+        }
+        [HttpGet, ActionName("Get")]
+        [Route("GetAll")]
+        public async Task<ActionResult<IEnumerable<Country>>> GetCountryAsync()
+        {
+            var countries = await _CountryService.GetCountryAsync();
+
+            if (countries == null || !countries.Any()) return NotFound();
+
+            return Ok(countries);
+        }
+
+        [HttpGet, ActionName("Get")]
+        [Route("GetById/{Id}")]
+        public async Task<ActionResult<Country>> GetCountryByIdAsync(Guid Id)
+        {
+            var country = await _CountryService.GetCountryByIdAsync(Id);
+
+            if (country == null) return NotFound();
+
+            return Ok(country);
+        }
+
+        [HttpPost, ActionName("Create")]
+        [Route("Create")]
+        public async Task<ActionResult<Country>> CreateCountryAsync(Country country)
+        {
+            try
+            {
+                var newCountry = await _CountryService.CreateCountryAsync(country);
+                if (newCountry == null) return NotFound();
+                return Ok(newCountry);
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.Contains("duplicate"))
+                    return Conflict(String.Format("{0} ya existe", country.Name));
+                return Conflict(ex.Message);
+            }
+        }
+
+        [HttpPut, ActionName("Edit")]
+        [Route("Edit")]
+        public async Task<ActionResult<Country>> EditCountryAsync(Country country)
+        {
+            try
+            {
+                var editedCountry = await _CountryService.EditCountryAsync(country);
+                if (editedCountry == null) return NotFound();
+                return Ok(editedCountry);
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.Contains("duplicate"))
+                    return Conflict(String.Format("{0} ya existe", country.Name));
+                return Conflict(ex.Message);
+            }
+        }
+
+        [HttpDelete, ActionName("Delete")]
+        [Route("Delete ")]
+        public async Task<ActionResult<Country>> DeleteCountryAsync(Guid Id)
+        {
+            if (Id == null) return BadRequest();
+            var deletedCountry = await _CountryService.DeleteCountryAsync(Id);
+            if (deletedCountry == null) return NotFound();
+            return Ok(deletedCountry);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using webapitest.DAL;
+using webapitest.Domain.Interfaces;
+using webapitest.Domain.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+
 builder.Services.AddDbContext<DateBaseContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<ICountryService, CountryService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
